c6272b11d7bf073e45ecfb53818f3aa27bbc0c6b
Carl Tanner <carl@wdwhub.net>
2016-01-01 20:52:51 -0800

    ADDED: Account hm Address

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,5 +1,4 @@
 class Account < ActiveRecord::Base
-  # has_many :addresses
-  # has_many :notes, 
   has_many :notes, as: :notable, :inverse_of => :notable
+  has_many :addresses, as: :addressable, :inverse_of => :addressable
 end
