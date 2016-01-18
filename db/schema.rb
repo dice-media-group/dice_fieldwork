a91014b24406603174ceba2e88ddf811e9f4ece6
Carl Tanner <carl@wdwhub.net>
2016-01-17 17:46:37 -0800

    ADDED: Account fields to schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160102040313) do
+ActiveRecord::Schema.define(version: 20160118014401) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "accounts", force: :cascade do |t|
-    t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
+    t.string   "first_name"
+    t.string   "last_name"
+    t.text     "email"
+    t.boolean  "no_customer_email_address"
+    t.string   "primary_phone_number"
+    t.string   "secondary_phone_number"
   end
 
   create_table "addresses", force: :cascade do |t|
@@ -32,6 +37,7 @@ ActiveRecord::Schema.define(version: 20160102040313) do
     t.string   "addressable_type"
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
+    t.boolean  "is_billing"
   end
 
   add_index "addresses", ["addressable_type", "addressable_id"], name: "index_addresses_on_addressable_type_and_addressable_id", using: :btree
