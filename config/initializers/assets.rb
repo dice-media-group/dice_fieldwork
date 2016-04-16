82ebc4cc7f475fd58099c53278737aaf634a2d84
Carl Tanner <rocketeer.captproton@gmail.com>
2016-04-16 12:14:11 -0700

    INIT: created payment box for service agreement

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,4 +8,4 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += %w( jquery.signaturepad.js json2.min jquery.signaturepad.css )
+Rails.application.config.assets.precompile += %w( jquery.signaturepad.js json2.min jquery.signaturepad.css service_agreement )
