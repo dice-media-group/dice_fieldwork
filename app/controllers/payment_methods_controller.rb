5447630cadd493b8561eff86a635d17ccc071fbd
Carl Tanner <rocketeer.captproton@gmail.com>
2016-04-30 07:31:50 -0700

    ADDED: account entry popup

--- app/controllers/payment_methods_controller.rb
+++ app/controllers/payment_methods_controller.rb
@@ -16,7 +16,7 @@ class PaymentMethodsController < ApplicationController
   private
 
   def load_parent
-    klass = [Account].detect { |c| params["#{c.name.underscore}_id"]}
+    klass = [Account, ServiceAgreement].detect { |c| params["#{c.name.underscore}_id"]}
     @parent  = klass.find(params["#{klass.name.underscore}_id"])
   end
     def payment_methods_params
