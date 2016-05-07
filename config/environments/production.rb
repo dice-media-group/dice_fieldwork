cb02533f7023e30566993b71a399b805db3465c1
Carl Tanner <rocketeer.captproton@gmail.com>
2016-05-07 14:34:21 -0700

    CHANGED: gmail config to sendgrid config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,26 +82,26 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { :host => 'lvh.me:3049' }
   
   
-  config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    port: 587,
-    domain: "gmail.com",
-    authentication: "plain",
-    enable_starttls_auto: true,
-    user_name: ENV["GMAIL_USERNAME"],
-    password: ENV["GMAIL_PASSWORD"]
-  }
-
-  # config.action_mailer.delivery_method = :smtp
-  # config.action_mailer.default_url_options = { host: ENV['HOST_URL']}
-  #
-  # ActionMailer::Base.smtp_settings = {
-  #   :address        => 'smtp.sendgrid.net',
-  #   :port           => '587',
-  #   :authentication => :plain,
-  #   :user_name      => ENV['SENDGRID_USERNAME'],
-  #   :password       => ENV['SENDGRID_PASSWORD'],
-  #   :domain         => 'heroku.com',
-  #   :enable_starttls_auto => true
+  # config.action_mailer.smtp_settings = {
+  #   address: "smtp.gmail.com",
+  #   port: 587,
+  #   domain: "gmail.com",
+  #   authentication: "plain",
+  #   enable_starttls_auto: true,
+  #   user_name: ENV["GMAIL_USERNAME"],
+  #   password: ENV["GMAIL_PASSWORD"]
   # }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: ENV['HOST_URL']}
+
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'herokuapp.com',
+    :enable_starttls_auto => true
+  }
 end
